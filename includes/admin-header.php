efa0d335347d44b40d185e139be660c7ceb97d97
Arjay <arjaymalaga990@gmail.com>
2023-02-10 11:49:13 +0800

    Update admin-header

--- includes/admin-header.php
+++ includes/admin-header.php
@@ -6,8 +6,12 @@
     <title>PHSI - Admin Dashboard</title>
     <link rel="stylesheet" href="../css/dashboard.css">
     <link rel="stylesheet" href="../css/jaydee.css"> 
-       <!-- Custom IconScunt for this template-->
-   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <!--<link rel ="styleshee" href="https://unpkg.com/aos@2.3.1/dist/aos.css">-->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <!-- Custom IconScunt for this template-->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 </head>
 <body>
