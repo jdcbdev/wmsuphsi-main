4e1d2b08266f685d4c49e0748fde5b4122023219
JerichoSagdi0 <117694616+JerichoSagdi0@users.noreply.github.com>
2023-02-23 09:06:18 +0800

    Update home.php

--- home.php
+++ home.php
@@ -243,8 +243,10 @@
 
  <!-- Swiper Js link  -->
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<!-- test  -->
 
 <!-- Custom Js file link  -->
+
 <script src="js/script.js"></script>
 
 </body>
