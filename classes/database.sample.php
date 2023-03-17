5d59b6430fcd299fc8ecf46de94f1a71eefc58f8
Arjay <arjaymalaga990@gmail.com>
2023-03-17 15:17:50 +0800

    update

---
+++ classes/database.sample.php
@@ -0,0 +1,33 @@
+<?php
+
+class Database{
+    private $host = 'localhost';
+
+    
+
+    // private $username = 'u654609850_';
+    // private $password = 'E1C2H3O4';
+    // private $database = 'u654609850_phsi';
+
+    private $username = 'root';
+    private $password = '';
+    private $database = 'phsi';
+    
+    protected $connection;
+
+    function connect(){
+        try 
+			{
+				$this->connection = new PDO("mysql:host=$this->host;dbname=$this->database", 
+											$this->username, $this->password);
+			} 
+			catch (PDOException $e) 
+			{
+				echo "Connection error " . $e->getMessage();
+			}
+        return $this->connection;
+    }
+
+}
+
+?>
