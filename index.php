73351b52cc1d67a23b18ca31e3108a91af88b24a
Arjay <arjaymalaga990@gmail.com>
2023-04-26 00:15:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     //check if user is login already otherwise send to home page yet not login)
     if (isset($_SESSION['role']) == 'super_admin' || ($_SESSION['role']) == 'unesco_admin' || ($_SESSION['role']) == 'phsi_admin' || ($_SESSION['role']) && $_SESSION['verified'] == 1){
-        header('location: admin/dashboard.php');
+        header('location: admin/admin.php');
     }
     else if (isset($_SESSION['role']) == 'user' && $_SESSION['verified'] == 1){
         header('location: user/user-profile.php');
