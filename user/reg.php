9bfc70aa0dfbe469f1f16bb66c06353deebca698
Arjay <arjaymalaga990@gmail.com>
2023-05-06 01:28:48 +0800

    up

--- user/reg.php
+++ user/reg.php
@@ -2,11 +2,12 @@
     $page_title = 'Registered Succesfully | WMSU - Peace and Human Security Institute';
     require_once '../includes/head.php';
     require_once '../includes/header.php';
+    $email = $_SESSION['email'];
 ?>
 
     <section class="container" style="">       
         <div>
-            <p style="font-size: 2em;">We are thrilled to have you join us at the event. Please check your email and download the QR Code we sent. See you soon!</p>
+            <p style="font-size: 2em;"><?php echo 'We are thrilled to have you join us at the event. Please check your email ['.$email.'] and download the QR Code we sent. See you soon!' ?></p>
         </div>         
         <div class="left-div" style="">
             <img  src="../images/content-images/qr.png" alt="" style="width: 50%; margin: auto; margin-bottom: 20px;">
