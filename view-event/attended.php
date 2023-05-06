23cb361c60da420d417709bb3e383c69b5b6886c
Arjay <arjaymalaga990@gmail.com>
2023-05-06 10:31:03 +0800

    Update attended.php

--- view-event/attended.php
+++ view-event/attended.php
@@ -3,7 +3,7 @@
         <thead>
             <tr>
                 <!--<th scope="col">#</th>-->
-                <th scope="col">Action</th>
+                <!--<th scope="col">Action</th>-->
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
                 <th scope="col">Type</th>
@@ -28,11 +28,11 @@
 ?>
 
         <tr>
-            <td>
+            <!--<td>
                 <div class="action-button">
                     <a name="confirm_attendance" type="submit" class="me-2 green" id="confirm_attendance" value="#"><i class='bx bx-send' ></i>Send Feedback Form</a>
                 </div>
-            </td>
+            </td>-->
             <td><?php echo $value['firstname'].' '.$value['middlename'].' '.$value['lastname'].' '.$value['suffix'] ?></td>
             <td><?php echo $value['email'] ?></td>
             <td><span class="type-Student"><?php echo $value['member_type']?></span></td>
